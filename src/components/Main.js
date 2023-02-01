94c2671bd619c105c7e1d3f3b3e2ba017894aa92
MathisPastreNelson <shiray@hotmail.fr>
2023-02-01 03:49:52 +0100

    Utilisation de Ant design OK

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,21 +2,17 @@ import React from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { Carousel } from 'antd';
+import { MobileOutlined, RiseOutlined, LoadingOutlined, DropboxOutlined } from '@ant-design/icons';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faEnvelope, faWonSign } from '@fortawesome/free-solid-svg-icons'
 
 export default function Main() {
-    let settings = {
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-    };
-
     return (
         <div className='main_container'>
-            <Slider {...settings}>
-                <div className='card_container'>
+
+            <Carousel>
+                <div>
                     <section className="present">
                         <div className="title">
                             <h2> Mes services ?</h2>
@@ -24,22 +20,36 @@ export default function Main() {
                         </div>
                         <div className="article">
                             <div>
-                                <h3>Intégration web</h3>
+                                <h3>
+                                    <DropboxOutlined /> Intégration web
+                                </h3>
                                 <p>Des intégrations HTML / CSS
                                     respectueuses des standards du Web.</p>
                             </div>
                             <div>
-                                <h3>Dynamisme des pages</h3>
+                                <h3>
+                                    <LoadingOutlined /> Dynamisme des pages
+                                </h3>
                                 <p>Des animations de contenu non intrusives pour embellir votre projet.</p>
                             </div>
                             <div>
-                                <h3>Référencement naturel</h3>
+                                <h3>
+                                    <RiseOutlined /> Référencement naturel
+                                </h3>
                                 <p>Affichage sémantique des informations,
                                     des pages propres pour un référencement optimal.</p>
                             </div>
                             <div>
                                 <h3>
-                                    Responsive design
+                                    <MobileOutlined /> Responsive design
+                                </h3>
+                                <p>
+                                    Compatible tous supports, tablette & application mobile.
+                                </p>
+                            </div>
+                            <div>
+                                <h3>
+                                    <MobileOutlined /> Développement récent
                                 </h3>
                                 <p>
                                     Compatible tous supports, tablette & application mobile.
@@ -48,24 +58,17 @@ export default function Main() {
                         </div>
                     </section>
                 </div>
+                <div>
+                    <h1 style={{ color: "white", lineHeight: "300px" }}>Slide 2</h1>
+                </div>
+                <div>
+                    <h1 style={{ color: "white", lineHeight: "300px" }}>Slide 3</h1>
+                </div>
+                <div>
+                    <h1 style={{ color: "white", lineHeight: "300px" }}>Slide 4</h1>
+                </div>
+            </Carousel >
+        </div>
+    )
 
-
-                <div className='card_container'>
-                    <h3>2</h3>
-                </div>
-                <div className='card_container'>
-                    <h3>3</h3>
-                </div>
-                <div className='card_container'>
-                    <h3>4</h3>
-                </div>
-                <div className='card_container'>
-                    <h3>5</h3>
-                </div>
-                <div className='card_container'>
-                    <h3>6</h3>
-                </div>
-            </Slider >
-        </div >
-    );
 }
