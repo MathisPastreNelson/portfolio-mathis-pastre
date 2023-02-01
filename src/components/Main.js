3063513b5b7864adb4f66a0f9cf78750286fc87e
MathisPastreNelson <shiray@hotmail.fr>
2023-02-01 16:45:47 +0100

    Impove style

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { useState } from 'react';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Carousel } from 'antd';
 import { MobileOutlined, RiseOutlined, LoadingOutlined, DropboxOutlined } from '@ant-design/icons';
+
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faEnvelope, faWonSign } from '@fortawesome/free-solid-svg-icons'
 
 export default function Main() {
+    const [autoplay, setAutoplay] = useState(true);
     return (
         <div className='main_container'>
-
-            <Carousel>
+            <Carousel autoplay={autoplay}>
                 <div>
                     <section className="present">
                         <div className="title">
@@ -66,7 +68,11 @@ export default function Main() {
                 <div>
                     <h1 style={{ color: "white", lineHeight: "300px" }}>Slide 4</h1>
                 </div>
+
             </Carousel >
+            <button className='buttonPlay' onClick={() => setAutoplay(!autoplay)}>
+                Défilement automatique  {autoplay ? "Off" : "On"}
+            </button>
         </div>
     )
 
