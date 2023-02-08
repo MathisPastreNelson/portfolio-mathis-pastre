84b1a313322124bcfbdebee49f03972a75df6127
MathisPastreNelson <shiray@hotmail.fr>
2023-02-08 14:18:33 +0100

    Ajout du 4 eme slide

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ import "slick-carousel/slick/slick-theme.css";
 import { Carousel } from 'antd';
 import { FloatButton } from 'antd';
 import { FaJs, FaPhp, FaCss3Alt, FaReact, FaSass, FaBootstrap, FaWordpress, FaHtml5, FaNetworkWired } from 'react-icons/fa';
-import { MobileOutlined, RiseOutlined, LoadingOutlined, DropboxOutlined, CodeOutlined, CaretRightOutlined, PauseOutlined } from '@ant-design/icons';
+import { MobileOutlined, RiseOutlined, LoadingOutlined, DropboxOutlined, CodeOutlined, CaretRightOutlined, PauseOutlined, LinkedinOutlined, GithubOutlined, MailOutlined } from '@ant-design/icons';
 import bookio from "../assets/bookio.png"
 import ohmyfood from "../assets/ohmyfood.png"
 import lapanthere from "../assets/lapanthere.png"
@@ -210,6 +210,33 @@ export default function Main() {
 
                     </section>
                 </div>
+                {/* Slide 4 */}
+                <div>
+                    <section className="present">
+                        <div className="title bgBanner2">
+                            <div>
+                                <h2>Me contacter</h2>
+                                <h2>Un projet de création web ?</h2>
+                            </div>
+                        </div>
+                        <div className="book_container bgBackground3 flexCenter">
+                            <a href="mailto:mathis.pastre@outlook.com" >
+                                <MailOutlined />
+                                <h3>Email</h3>
+                            </a>
+                            <a href="https://www.linkedin.com/in/mathis-pastre/">
+                                <LinkedinOutlined />
+                                <h3>Linkdin</h3>
+                            </a>
+                            <a href="https://github.com/MathisPastreNelson" >
+                                <GithubOutlined />
+                                <h3>Github</h3>
+                            </a>
+                        </div>
+
+                    </section>
+                </div>
+
 
             </Carousel >
             {
