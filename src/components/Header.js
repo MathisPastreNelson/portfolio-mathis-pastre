dc74395bb080fbd1a9990b79ea20a9e2f2a4258d
MathisPastreNelson <shiray@hotmail.fr>
2023-02-08 14:51:36 +0100

    Ajout logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import logo from "../assets/logo.png"
 
 export default function Header() {
     return (
         <header className="App-header">
+            <img src={logo} className="imgLogo" alt="logo" />
             <div className="content">
                 <h1>Mathis Pastre Développeur Web</h1>
             </div>
