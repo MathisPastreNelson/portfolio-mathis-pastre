db8bf2dbf6876d3d2d790122e416cfced47fdbfa
MathisPastreNelson <shiray@hotmail.fr>
2023-04-04 15:41:19 +0200

    mouse component ajout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import MouseFollower from "./components/MouseFollower";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MouseFollower />
       <Main />
       <Footer />
     </div>
