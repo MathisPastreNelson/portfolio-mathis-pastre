16d5c2815b590702a97e307e64094674833d4acc
MathisPastreNelson <shiray@hotmail.fr>
2023-04-04 17:29:41 +0200

    amélioration mes compétences

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -29,64 +29,74 @@ export default function Presentation() {
       <div className="article bgBackground2">
         <div className="icons_container">
           <div>
-            <div className="iconAnimation iconBloc">
-              <FaHtml5 className="skillIcons htmlIcon bgIcon" />
-              <div className="tooltip">HTML</div>
-            </div>
-            <div className="iconAnimation iconBloc">
-              <FaCss3Alt className="skillIcons cssIcon bgIcon" />
-              <div className="tooltip">CSS</div>
-            </div>
-            <div className="iconAnimation iconBloc">
-              <FaJs className="skillIcons jsIcon bgIcon" />
-              <div className="tooltip">Javascript</div>
-            </div>
-          </div>
-          <div>
-            <div className="iconAnimation iconBloc">
-              <FaPhp className="skillIcons phpIcon bgIcon" />
-              <div className="tooltip">PHP</div>
-            </div>
-
-            <div className="iconAnimation iconBloc">
-              <FaReact className="skillIcons reactIcon bgIcon" />
-              <div className="tooltip">React</div>
-            </div>
-            <div className="iconAnimation iconBloc">
-              <FaSass className="skillIcons sassIcon bgIcon" />
-              <div className="tooltip">Sass</div>
+            <h3 className="titleOfSkills">Languages</h3>
+            <div className="icons_container_lineDisplay">
+              <div className="iconAnimation iconBloc">
+                <FaHtml5 className="skillIcons htmlIcon bgIcon" />
+                <div className="tooltip">HTML</div>
+              </div>
+              <div className="iconAnimation iconBloc">
+                <FaCss3Alt className="skillIcons cssIcon bgIcon" />
+                <div className="tooltip">CSS</div>
+              </div>
+              <div className="iconAnimation iconBloc">
+                <FaJs className="skillIcons jsIcon bgIcon" />
+                <div className="tooltip">Javascript</div>
+              </div>
+              <div className="iconAnimation iconBloc">
+                <FaPhp className="skillIcons phpIcon bgIcon" />
+                <div className="tooltip">PHP</div>
+              </div>
             </div>
           </div>
           <div>
-            <div className="iconAnimation iconBloc">
-              <FaBootstrap className="skillIcons bsIcon bgIcon" />
-              <div className="tooltip">Bootstrap</div>
-            </div>
-            <div className="iconAnimation iconBloc">
-              <FaWordpress className="skillIcons wpIcon bgIcon" />
-              <div className="tooltip">WordPress</div>
-            </div>
-            <div className="iconAnimation iconBloc">
-              <img className="figmaIcon wpIcon bgIcon" src={figmaIcon} alt="" />
-              <div className="tooltip">FIGMA</div>
+            <h3 className="titleOfSkills">Frameworks/Bibliothèques</h3>
+            <div className="icons_container_lineDisplay">
+              <div className="iconAnimation iconBloc">
+                <FaReact className="skillIcons reactIcon bgIcon" />
+                <div className="tooltip">React</div>
+              </div>
+              <div className="iconAnimation iconBloc">
+                <FaNode className="skillIcons nodejsIcon bgIcon" />
+                <div className="tooltip">NodeJS</div>
+              </div>
+              <div className="iconAnimation iconBloc">
+                <FaBootstrap className="skillIcons bsIcon bgIcon" />
+                <div className="tooltip">Bootstrap</div>
+              </div>
+              <div className="iconAnimation iconBloc">
+                <FaSass className="skillIcons sassIcon bgIcon" />
+                <div className="tooltip">Sass</div>
+              </div>
             </div>
           </div>
           <div>
-            <div className="iconAnimation iconBloc">
-              <img
-                className="figmaIcon mongodbIcon bgIcon"
-                src={mongodbIcon}
-                alt=""
-              />
-              <div className="tooltip">MongoDB</div>
-            </div>
-            <div className="iconAnimation iconBloc">
-              <FaNode className="skillIcons nodejsIcon bgIcon" />
-              <div className="tooltip">NodeJS</div>
-            </div>
-            <div className="iconAnimation iconBloc">
-              <FaNetworkWired className="apiIcon wpIcon bgIcon" />
-              <div className="tooltip">API</div>
+            <h3 className="titleOfSkills">Outils</h3>
+            <div className="icons_container_lineDisplay">
+              <div className="iconAnimation iconBloc">
+                <FaNetworkWired className="apiIcon wpIcon bgIcon" />
+                <div className="tooltip">API</div>
+              </div>
+              <div className="iconAnimation iconBloc">
+                <img
+                  className="figmaIcon mongodbIcon bgIcon"
+                  src={mongodbIcon}
+                  alt=""
+                />
+                <div className="tooltip">MongoDB</div>
+              </div>
+              <div className="iconAnimation iconBloc">
+                <img
+                  className="figmaIcon wpIcon bgIcon"
+                  src={figmaIcon}
+                  alt=""
+                />
+                <div className="tooltip">FIGMA</div>
+              </div>
+              <div className="iconAnimation iconBloc">
+                <FaWordpress className="skillIcons wpIcon bgIcon" />
+                <div className="tooltip">WordPress</div>
+              </div>
             </div>
           </div>
         </div>
