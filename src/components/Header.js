c5970b42672652479c655547039e3554e0e4bb9f
MathisPastreNelson <shiray@hotmail.fr>
2023-04-08 21:14:06 +0200

    H1 animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,17 @@ export default function Header() {
   return (
     <header className="App-header">
       <img src={logo} className="imgLogo" alt="logo" />
-      <div className="content">
-        <h1>Mathis Pastre Développeur Web</h1>
+      <div class="container">
+        <div class="box">
+          <div class="titleh1">
+            <h1>Mathis Pastre</h1>
+          </div>
+
+          <div class="role">
+            <div class="block"></div>
+            <p>Développeur Web</p>
+          </div>
+        </div>
       </div>
     </header>
   );
