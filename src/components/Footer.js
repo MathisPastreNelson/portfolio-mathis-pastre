91b00c4fe1bbbeb677e10ec10e7f1160f0a5acdb
MathisPastreNelson <shiray@hotmail.fr>
2023-04-12 15:19:40 +0200

    ajout de target blank lien footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,11 +13,17 @@ export default function Footer() {
         <MailOutlined />
         <h3>Email</h3>
       </a>
-      <a href="https://www.linkedin.com/in/mathis-pastre/">
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.linkedin.com/in/mathis-pastre/">
         <LinkedinOutlined />
         <h3>Linkdin</h3>
       </a>
-      <a href="https://github.com/MathisPastreNelson">
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/MathisPastreNelson">
         <GithubOutlined />
         <h3>Github</h3>
       </a>
